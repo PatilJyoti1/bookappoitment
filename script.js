38c13e621c5f057c906b97c7bd71fb032113635b
PatilJyoti1 <jyotipatil616861@gmail.com>
2023-06-28 04:46:38 +0000

    edit Button

--- script.js
+++ script.js
@@ -9,44 +9,76 @@ function saveAppointments() {
 // Function to add an appointment
 function addAppointment(event) {
   event.preventDefault();
-  
+
   // Get the form values
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const phone = document.getElementById('phone').value;
-  
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const phoneInput = document.getElementById('phone');
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const phone = phoneInput.value;
+
   // Create an appointment object
   const appointment = {
     name,
     email,
     phone
   };
-  
+
   // Add the appointment to the array
   appointments.push(appointment);
-  
+
   // Save the updated appointments to LocalStorage
   saveAppointments();
-  
+
   // Display the appointment on the screen
   displayAppointment(appointment);
-  
+
   // Clear the form
-  document.getElementById('bookingForm').reset();
+  nameInput.value = '';
+  emailInput.value = '';
+  phoneInput.value = '';
 }
 
 // Function to delete an appointment
 function deleteAppointment(index) {
   // Remove the appointment from the array
   appointments.splice(index, 1);
-  
+
   // Save the updated appointments to LocalStorage
   saveAppointments();
-  
+
   // Clear the appointments list on the screen
   const appointmentsList = document.getElementById('appointments');
   appointmentsList.innerHTML = '';
-  
+
+  // Display the remaining appointments on the screen
+  appointments.forEach(displayAppointment);
+}
+
+// Function to edit an appointment
+function editAppointment(index) {
+  // Retrieve the appointment from the array
+  const appointment = appointments[index];
+
+  // Populate the input fields with the appointment's values
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const phoneInput = document.getElementById('phone');
+  nameInput.value = appointment.name;
+  emailInput.value = appointment.email;
+  phoneInput.value = appointment.phone;
+
+  // Remove the appointment from the array
+  appointments.splice(index, 1);
+
+  // Save the updated appointments to LocalStorage
+  saveAppointments();
+
+  // Clear the appointments list on the screen
+  const appointmentsList = document.getElementById('appointments');
+  appointmentsList.innerHTML = '';
+
   // Display the remaining appointments on the screen
   appointments.forEach(displayAppointment);
 }
@@ -54,16 +86,17 @@ function deleteAppointment(index) {
 // Function to display an appointment on the screen
 function displayAppointment(appointment, index) {
   const appointmentsList = document.getElementById('appointments');
-  
+
   const listItem = document.createElement('li');
   listItem.innerHTML = `
     <strong>Name:</strong> ${appointment.name}<br>
     <strong>Email:</strong> ${appointment.email}<br>
     <strong>Phone:</strong> ${appointment.phone}<br>
+    <button onclick="editAppointment(${index})">Edit</button>
     <button onclick="deleteAppointment(${index})">Delete</button>
     <hr>
   `;
-  
+
   appointmentsList.appendChild(listItem);
 }
 
@@ -72,4 +105,3 @@ document.getElementById('bookingForm').addEventListener('submit', addAppointment
 
 // Display existing appointments on page load
 appointments.forEach(displayAppointment);
-
